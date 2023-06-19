e96c2fbca92d8b235e466bf4fb0e41266f5f446f
alstjrdlzz <tjalstjr95@gmail.com>
2023-06-19 21:30:51 +0900

    feat: add __init__ files

--- wngPdlsDB/document/__init__.py
+++ wngPdlsDB/document/__init__.py
@@ -1,2 +1,5 @@
 from wngPdlsDB.document.playlist import Playlist
 from wngPdlsDB.document.tag_document import TagDocument
+from wngPdlsDB.document.song_document import SongDocument
+from wngPdlsDB.document.artist_document import ArtistDocument
+from wngPdlsDB.document.album_document import AlbumDocument
