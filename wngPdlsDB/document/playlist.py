d4d7113b7885ca6c95a0ed8f5569b3e5a9253606
2jun0 <soo28819@gmail.com>
2023-06-19 12:23:37 +0900

    feat: add option in playlist col

--- wngPdlsDB/document/playlist.py
+++ wngPdlsDB/document/playlist.py
@@ -1,10 +1,11 @@
 from mongoengine import Document, StringField, IntField, DateTimeField
 
+
 class Playlist(Document):
-    genie_id = IntField(required=True)
+    genie_id = IntField(required=True, unique=True)
     title = StringField(required=True)
     description = StringField(required=True)
-    likes = IntField(required=True)
-    views = IntField(required=True)
+    likes = IntField(required=True, min_value=0)
+    views = IntField(required=True, min_value=0)
     created_date = DateTimeField(required=True)
     updated_date = DateTimeField(required=True)
