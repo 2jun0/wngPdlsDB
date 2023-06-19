b1be83f09749661fd3753a377f57beca47cb5d4f
2jun0 <soo28819@gmail.com>
2023-06-19 17:49:45 +0900

    fix: add self param in TagRepository.create_tag

--- wngPdlsDB/repository/tag_repository.py
+++ wngPdlsDB/repository/tag_repository.py
@@ -2,6 +2,6 @@ from wngPdlsDB.document import Tag
 
 
 class TagRepository:
-    def create_tag(genie_id: str, title: str):
+    def create_tag(self, genie_id: str, title: str):
         tag = Tag(genie_id=genie_id, title=title)
         tag.save()
