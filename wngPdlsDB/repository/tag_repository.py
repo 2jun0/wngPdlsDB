288da5d2ac29be293efdf5fd7851c1ff349a20af
2jun0 <soo28819@gmail.com>
2023-06-19 18:07:53 +0900

    feat: return Tag in create_tag

--- wngPdlsDB/repository/tag_repository.py
+++ wngPdlsDB/repository/tag_repository.py
@@ -2,6 +2,7 @@ from wngPdlsDB.document import Tag
 
 
 class TagRepository:
-    def create_tag(self, genie_id: str, title: str):
+    def create_tag(self, genie_id: str, title: str) -> Tag:
         tag = Tag(genie_id=genie_id, title=title)
-        tag.save()
+        return tag.save()
+        
