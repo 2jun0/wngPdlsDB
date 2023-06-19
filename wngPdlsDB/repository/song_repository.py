d5ed4e55af76f585b9605d6baa1f93f6204f2170
alstjrdlzz <tjalstjr95@gmail.com>
2023-06-20 00:26:34 +0900

    feat: find by artist and album

--- wngPdlsDB/repository/song_repository.py
+++ wngPdlsDB/repository/song_repository.py
@@ -26,6 +26,16 @@ class SongRepository:
         song: SongDocument = SongDocument.objects(genie_id=genie_id).first()
         return song.to_dto()
 
+    def find_by_artist(self, artist: ArtistDto) -> list[SongDto]:
+        artist_doc: ArtistDocument = self.__find_artist_doc_by_dto(artist)
+        songs: list[SongDocument] = SongDocument.objects(artist=artist_doc)
+        return [song.to_dto() for song in songs]
+
+    def find_by_album(self, album: AlbumDto) -> list[SongDto]:
+        album_doc: AlbumDocument = self.__find_album_doc_by_dto(album)
+        songs: list[SongDocument] = SongDocument.objects(album=album_doc)
+        return [song.to_dto() for song in songs]
+
     def find_all(self) -> list[SongDto]:
         songs: list[SongDocument] = list(SongDocument.objects)
         return [song.to_dto() for song in songs]
