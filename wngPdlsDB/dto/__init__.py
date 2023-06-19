ca6204124bf086efb6bda03e4878d2243e6b87b8
2jun0 <soo28819@gmail.com>
2023-06-20 00:30:34 +0900

    fix: remove duplicated import

--- wngPdlsDB/dto/__init__.py
+++ wngPdlsDB/dto/__init__.py
@@ -3,4 +3,3 @@ from wngPdlsDB.dto.playlist_dto import PlaylistDto
 from wngPdlsDB.dto.song_dto import SongDto
 from wngPdlsDB.dto.artist_dto import ArtistDto
 from wngPdlsDB.dto.album_dto import AlbumDto
-from wngPdlsDB.dto.playlist_dto import PlaylistDto
