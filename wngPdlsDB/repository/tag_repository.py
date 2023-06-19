f637a7e59729185cbc77e58c2875ad4764ef75f9
2jun0 <soo28819@gmail.com>
2023-06-20 02:14:55 +0900

    feat: modi tag_repository's functions

--- wngPdlsDB/repository/tag_repository.py
+++ wngPdlsDB/repository/tag_repository.py
@@ -1,5 +1,6 @@
 from wngPdlsDB.document import TagDocument
-from wngPdlsDB.dto.tag_dto import TagDto
+from wngPdlsDB.exception import NotFoundTagException
+from wngPdlsDB.dto import TagDto
 
 
 class TagRepository:
@@ -8,11 +9,20 @@ class TagRepository:
         saved: TagDocument = tag.save()
         return saved.to_dto()
 
-    def delete_tag(self, genie_id: str) -> None:
-        self.find_by_genie_id(genie_id).delete()
+    def delete_by_genie_id(self, genie_id: str) -> None:
+        query_set = TagDocument.objects(genie_id=genie_id)
 
-    def find_by_genie_id(self, genie_id: str) -> TagDto:
+        if not query_set:
+            raise NotFoundTagException(f"Can't find tag document: genie_id={genie_id}")
+
+        query_set.delete()
+
+    def find_by_genie_id(self, genie_id: str) -> TagDto | None:
         tag: TagDocument = TagDocument.objects(genie_id=genie_id).first()
+
+        if not tag:
+            return None
+
         return tag.to_dto()
 
     def find_all(self) -> list[TagDto]:
