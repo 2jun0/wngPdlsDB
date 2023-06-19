df6d2e1bf16565412611a58758b23d7a930ab83f
2jun0 <soo28819@gmail.com>
2023-06-19 21:16:39 +0900

    feat: add playlist doc to dto

--- wngPdlsDB/document/playlist_document.py
+++ wngPdlsDB/document/playlist_document.py
@@ -1,4 +1,5 @@
 from mongoengine import Document, StringField, IntField, DateTimeField
+from wngPdlsDB.dto import PlaylistDto
 
 
 class PlaylistDocument(Document):
@@ -9,3 +10,14 @@ class PlaylistDocument(Document):
     views = IntField(required=True)
     created_date = DateTimeField(required=True)
     updated_date = DateTimeField(required=True)
+
+    def to_dto(self) -> PlaylistDto:
+        return PlaylistDto(
+            self.genie_id,
+            self.title,
+            self.description,
+            self.likes,
+            self.views,
+            self.created_date,
+            self.updated_date,
+        )
