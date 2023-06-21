19e3b0b00e1f4d5ce310caf42b654d2444121f1e
2jun0 <soo28819@gmail.com>
2023-06-21 11:48:58 +0900

    feat: add validation should have at last one elm in doc

--- wngPdlsDB/document/tagged_image_document.py
+++ wngPdlsDB/document/tagged_image_document.py
@@ -1,11 +1,20 @@
-from mongoengine import Document, URLField, ReferenceField, ListField
+from mongoengine import Document, URLField, ReferenceField, ListField, ValidationError
 from wngPdlsDB.dto import TaggedImageDto
 from wngPdlsDB.document.tag_document import TagDocument
 
 
+def should_have_at_least_one_tag(tags: list[TagDocument]):
+    if len(tags) <= 0:
+        raise ValidationError(f"TaggedImage should have at least one tags: {tags}")
+
+
 class TaggedImageDocument(Document):
     url = URLField(required=True)
-    tags = ListField(ReferenceField(TagDocument), required=True)
+    tags = ListField(
+        ReferenceField(TagDocument),
+        required=True,
+        validation=should_have_at_least_one_tag,
+    )
 
     def to_dto(self) -> TaggedImageDto:
         return TaggedImageDto(
